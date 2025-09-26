135e3e5658441211a56fbbe82c36bfad216782bc
jrbistuer <jrbistuer@gmail.com>
2025-09-26 15:18:57 +0200

    capacitor barcode scanner

--- src/app/private/tab3/tab3.page.ts
+++ src/app/private/tab3/tab3.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerOptions, CapacitorBarcodeScannerScanResult, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Tab3Page {
+
+  options: CapacitorBarcodeScannerOptions = {
+    hint: CapacitorBarcodeScannerTypeHint.QR_CODE,
+  };
+  scanResult = '';
+
   constructor() {}
+
+  scanCode() {
+    console.log('Scanning barcode...');
+    CapacitorBarcodeScanner.scanBarcode(this.options).then((result: CapacitorBarcodeScannerScanResult) => {
+      console.log('Scan result:', result);
+      this.scanResult = result.ScanResult || 'No result';
+    }).catch(error => {
+      console.error('Scan error:', error);
+    });
+  }
+
 }
