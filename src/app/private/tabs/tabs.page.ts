cb869c8cb6829a85e680007df5cefb9962392315
jrbistuer <jrbistuer@gmail.com>
2025-09-27 11:44:26 +0200

    push notifications

--- src/app/private/tabs/tabs.page.ts
+++ src/app/private/tabs/tabs.page.ts
@@ -2,6 +2,7 @@ import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
+import { PushService } from 'src/app/services/push.service';
 
 @Component({
   selector: 'app-tabs',
@@ -11,8 +12,15 @@ import { triangle, ellipse, square } from 'ionicons/icons';
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
+  public pushService = inject(PushService);
 
   constructor() {
     addIcons({ triangle, ellipse, square });
+    this.pushService.registerNotifications().then(() => {
+      this.pushService.addListeners();
+    }).catch(err => {
+      console.error('Error registering for push notifications', err);
+    });
   }
+  
 }
